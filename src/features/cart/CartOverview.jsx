d852e078e203e18741ba468bb7d3200fa067a329
Hemakrishna <jayavarapukrishna211@gmail.com>
2024-03-31 18:26:29 +0530

    update

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -7,6 +7,7 @@ function CartOverview() {
   const noOfPizzas = useSelector(getTotalCartQuantity);
   const totalPrice = useSelector(getTotalCartPrice);
   if (!noOfPizzas) return null;
+
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
